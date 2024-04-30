b684013ce0274fa4852dc44b90b1bca378f15dda
Welinton Yoshio Hirai <wyhirai@gmail.com>
2024-04-30 16:05:29 -0300

    atualizacao de imagens

--- code.R
+++ code.R
@@ -1,9 +1,3 @@
----
-title: "Mapas"
-output: html_document
----
-
-```{r}
 # Header ------------------------------------------------------------------
 # Hospital de Amor [https://hospitaldeamor.com.br/site/]
 # Núcleo de Epidemiologia e Bioestatística (NEB) [https://iep.hospitaldeamor.com.br/plataformas-de-apoio/neb/]
@@ -12,32 +6,35 @@ output: html_document
 #limpando ambiente
 rm(list = ls())
 
+setwd("C:/Users/HANEB02/Documents/GitHub/Mapa---SP")
+
 #bibliotecas utilizadas
 library(magrittr)
 library(dplyr)
 library(geobr)
 library(sf)
 library(ggplot2)
-```
 
-```{r}
 #baixando os dados
 data_muni <- geobr::read_municipality(code_muni = 35)
 
+# png('imagens/mapa1.png', width = 2400, height = 1200, res = 200)
 #gerando o mapa
 data_muni %>% 
   ggplot() +
   geom_sf() +
   theme_void()
+# dev.off()
+
+# png('imagens/mapa2.png', width = 2400, height = 1200, res = 200)
 
 #indetificandos apenas a cidade de Barretos
 data_muni %>% 
-  dplyr::mutate(Indicadora = if_else(name_muni == 'Barretos', 1, 0) %>% 
+  dplyr::mutate(Indicadora = if_else(name_muni %in% c('Barretos', 'Lavínia'), 1, 0) %>% 
                   as.factor()) %>% 
   ggplot() +
   geom_sf(aes(fill = Indicadora)) +
   theme_void() +
   scale_fill_manual(values = c('white', 'skyblue'))
 
-```
-
+# dev.off()
